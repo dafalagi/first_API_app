c4d79c8f3217e8aa6127af2411847c79704932ae
dafalagi <dafarizky34@gmail.com>
2022-01-22 19:44:17 +0700

    test

--- scratchs/test.php
+++ scratchs/test.php
@@ -1,21 +1,4 @@
 <?php
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, 'https://v2.convertapi.com/convert/jpg/to/webp?Secret=ewcp8KOU5rfx9aKO');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$post = array(
-    'File' => 'http://image.tmdb.org/t/p/original/bwBmo4I3fqMsVvVtamyVJHXGnLF.jpg'
-);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-curl_close($ch);
+   $a=25;
+   echo ($a>=10?$a>20?50:30:$a);
 ?>
-<img src="<?php echo $result["Files"] ?>" alt="">
-
-
